04040336816bc4d956a4b0d8880cbd1ec08965d5
Ryan Rodwell <ryanrodwell@gmail.com>
2019-02-04 15:43:28 -0500

    removing log and changing forecast range

--- src/components/app.js
+++ src/components/app.js
@@ -24,7 +24,7 @@ class App extends Component {
     getWeatherFromYahoo(location).then(weather =>{
       console.log("Weather",weather);
       let upcomingWeather = [];
-      for(var i=0;i<7;i++){
+      for(var i=1;i<8;i++){
          upcomingWeather.push(weather.forecasts[i])
       }
       this.setState({
--- src/services/api_services.js
+++ src/services/api_services.js
@@ -1,14 +1,9 @@
+import OAuth from 'oauth';
 import yahoo from '../config/yahoo_weather_creds';
 
-console.log(yahoo);
 export const getWeatherFromYahoo = (location) =>{
-    var OAuth = require('oauth');
-    
     return new Promise((resolve, reject)=>{
-
-        let header = {
-            "Yahoo-App-Id": yahoo.app_id
-        };
+        let header = {"Yahoo-App-Id": yahoo.app_id};
 
         let request = new OAuth.OAuth(null,null,yahoo.client_id,yahoo.client_secret,'1.0',null,'HMAC-SHA1',null,header);
 
